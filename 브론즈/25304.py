f79914d1c6dd8952571f65ba9c9ed4b70e2ad6de
comppy <kim1122aass@gmail.com>
2025-10-22 01:39:12 +0900

    25304

---
+++ 브론즈/25304.py
@@ -0,0 +1,10 @@
+X = int(input())
+N = int(input())
+total_sum = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    total_sum += (a * b)
+if(total_sum == X):
+    print("Yes")
+else:
+    print("No") 
