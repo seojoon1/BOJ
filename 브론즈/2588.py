1da9c0587ada5316326883fde977f3146cee7aa2
comppy <kim1122aass@gmail.com>
2025-10-21 23:53:14 +0900

    2588

---
+++ 브론즈/2588.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+num2_list = list(map(int, str(num2)))
+print(num1 * num2_list[2])
+print(num1 * num2_list[1])
+print(num1 * num2_list[0])
+print(num1 * num2)
